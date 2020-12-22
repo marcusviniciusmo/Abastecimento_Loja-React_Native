efa4e9af72e6e62f9dc3f6294eb9000627432cbd
Marcus Vinícius <marcusoliveira@pmenos.com.br>
2020-12-22 11:42:12 -0300

    Criação da tela Forçar Encerramento

---
+++ Abastecimento/src/Views/ForcarEncerramento/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { ContainerPrincipal, Container } from '../../AppStyles';
+import HeaderView from '../../Components/HeaderView';
+import Input from '../../Components/Input';
+import TipBottomView from '../../Components/TipBottomView';
+import { sizeDefaultInput, heightDefaultInput } from '../../Utils';
+
+const ForcarEncerramento: React.FC = () => {
+    const headerText = 'Forçar Encerramento';
+    const tipText = 'Informe todos os campos para continuar';
+
+    const [placaVeiculo, setPlacaVeiculo] = useState('');
+    const [numeroPedido, setNumeroPedido] = useState('');
+    const [filial, setFilial] = useState('');
+
+    return (
+        <ContainerPrincipal>
+            <HeaderView
+                title={headerText}
+                iconBack='arrow-left'
+                settings='more-vertical'
+            />
+            <Container>
+                <Input
+                    name='placa-veiculo'
+                    icon='truck'
+                    placeholder={'Placa do veículo'}
+                    onChangeText={setPlacaVeiculo}
+                    size={sizeDefaultInput}
+                    height={heightDefaultInput}
+                    bordered
+                />
+                <Input
+                    name='numero-pedido'
+                    icon='hash'
+                    placeholder={'Número do pedido'}
+                    onChangeText={setNumeroPedido}
+                    size={sizeDefaultInput}
+                    height={heightDefaultInput}
+                    bordered
+                />
+                <Input
+                    name='filial'
+                    icon='home'
+                    placeholder={'Filial'}
+                    onChangeText={setFilial}
+                    size={sizeDefaultInput}
+                    height={heightDefaultInput}
+                    bordered
+                />
+            </Container>
+            <TipBottomView
+                text={tipText}
+            />
+        </ContainerPrincipal>
+    )
+};
+
+export default ForcarEncerramento;
